3daccbe4696f6f36cbc747f872ad448209a7faba
9xEzreaL <goodmen5488@gmail.com>
2024-12-24 15:59:28 +0800

    Code refact.

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -39,3 +39,60 @@ def read_json_to_args(json_file):
         args = json.load(f)
     args = argparse.Namespace(**args)
     return args
+
+def get_gan_out(args, x0, model):
+    #x0 = [x.unsqueeze(0).unsqueeze(0).float().permute(0, 1, 3, 4, 2) for x in x0]
+    #x0 = torch.cat(x0, dim=1).cuda()  # concatenate all the input channels
+    # (Z, C, X, Y)
+    gpu = args.gpu
+    x0 = x0.permute(1, 2, 3, 0).unsqueeze(0)
+    if gpu:
+        x0 = x0.cuda()
+
+    if 0:
+        z = model(x0, method='encode')[-1].detach().cpu()
+        if gpu:
+            z = z.cuda()
+        XupX = model(z, method='decode')['out0'].detach().cpu()
+        XupX = XupX[0, 0, :, :, :]  # (X, Y, Z)
+
+    with torch.cuda.amp.autocast(enabled=args.fp16):
+        XupX = model(x0)['out0'].detach().cpu()[0, 0, :, :, :]   # out0 for original, out1 for psuedo-mask
+
+    Xup = x0.detach().cpu().squeeze()#.numpy()
+
+    XupX = XupX.permute(2, 0, 1)#torch.transpose(XupX, (2, 0, 1))  # (Z, X, Y)
+    Xup = Xup.permute(2, 0, 1)#torch.transpose(Xup, (2, 0, 1))
+
+    return XupX, Xup
+
+
+def get_ae_out(args, x0, model):
+    hbranchz = args.hbranchz
+    gpu = args.gpu
+    if gpu:
+        x0 = x0.cuda()
+    hb_all = []
+    for z in range(0, 32, 4):
+        with torch.cuda.amp.autocast(enabled=args.fp16):
+            reconstructions, posterior, hbranch = model.forward(x0[z:z+4, :, :, :], sample_posterior=False)
+        if hbranchz:
+            hb_all.append(posterior.sample())
+        else:
+            hb_all.append(hbranch)
+    hbranch = torch.cat(hb_all, dim=0).detach().cpu()
+    del hb_all
+
+    if hbranchz:
+        hbranch = model.decoder.conv_in(hbranch.cuda(0))
+
+    hbranch = hbranch.permute(1, 2, 3, 0).unsqueeze(0)
+    if gpu:
+        hbranch = hbranch.cuda()
+    XupX = model.net_g(hbranch, method='decode')['out0']  # (1, C, X, Y, Z)
+
+    Xup = torch.nn.Upsample(size=(256, 256, 256), mode='trilinear')(x0.permute(1, 2, 3, 0).unsqueeze(0))  # (1, C, X, Y, Z)
+    Xup = Xup[0, 0, ::].permute(2, 0, 1).detach().cpu()#.numpy()  # (Z, X, Y))
+    XupX = XupX[0, 0, ::].permute(2, 0, 1).detach().cpu()#.numpy()
+
+    return XupX, Xup
